2a1ac9474f4f23e41812a70d1ebe368c3fd654d0
Leonardo F. dos Santos <44267124+qleonardolp@users.noreply.github.com>
2026-07-12 13:20:21 -0300

    Add setup for zspace identification

--- launch/bravo7_simulation.launch.py
+++ launch/bravo7_simulation.launch.py
@@ -17,7 +17,7 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch_ros.actions import LifecycleNode, Node
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -56,6 +56,7 @@ def generate_launch_description():
         [package_share, 'worlds', [LaunchConfiguration('world'), '.sdf']]
     )
     controllers_config = PathJoinSubstitution([package_share, 'config', 'controllers.yaml'])
+    generator_config = PathJoinSubstitution([package_share, 'config', 'generators.yaml'])
     rviz_config = PathJoinSubstitution([package_share, 'config', 'bravo_rviz.rviz'])
 
     # Gazebo launch
@@ -132,11 +133,21 @@ def generate_launch_description():
         executable='spawner',
         arguments=[
             *controllers_list,
-            '--inactive',
+            '--activate-as-group',
             '--param-file',
             controllers_config,
         ],
     )
+
+    reference_generator = LifecycleNode(
+        package='robot_impedance_analyzer',
+        executable='kinematic_reference',
+        name='bravo7_reference',
+        namespace='',
+        autostart=True,
+        parameters=[generator_config],
+        )
+
     rviz = Node(
         package='rviz2',
         executable='rviz2',
@@ -153,6 +164,7 @@ def generate_launch_description():
         gazebo_spawner,
         broadcaster_spawner,
         controllers_spawner,
+        reference_generator,
         rviz,
     ]
 
